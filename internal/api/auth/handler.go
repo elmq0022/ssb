6849203b29d4b92a866751213c55bad2a958278a
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-08-31 14:20:54 -0500

    continuing to build the auth package - things are currently broken

--- internal/api/auth/handler.go
+++ internal/api/auth/handler.go
@@ -3,13 +3,11 @@ package auth
 import (
 	"encoding/json"
 	"errors"
-	"github.com/golang-jwt/jwt/v5"
 	"net/http"
 	"ssb/internal/pkg/auth"
 	"ssb/internal/pkg/router"
 	"ssb/internal/repo"
 	"ssb/internal/schemas"
-	"time"
 )
 
 func NewRouter(ur repo.UserRepository) *router.Router {
@@ -31,31 +29,14 @@ func NewRouter(ur repo.UserRepository) *router.Router {
 
 		match, err := auth.CheckPassword(password, u.HashedPassword)
 		if match && u.IsActive {
-			// TODO: but in its own function
-			//ref: https://pkg.go.dev/github.com/golang-jwt/jwt/v5#example-New-Hmac
-			now := time.Now().UTC()
-			exp := now.Add(1 * time.Hour)
-			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-				"sub": username,
-				"iss": "ssb-auth-service",
-				"aud": "ssb-backend",
-				"iat": now.Unix(),
-				"nbf": now.Unix(),
-				"exp": exp.Unix(),
-			})
-
-			tokenString, err := token.SignedString([]byte("sample-secret"))
+			token, err := auth.GenerateJWT(
+				username,
+				auth.WithAudience("ssb"))
 			if err != nil {
-				return "", http.StatusInternalServerError, err
+				return schemas.JsonToken{}, http.StatusInternalServerError, err
 			}
-
-			jwtToken := schemas.JsonToken{
-				Token: tokenString,
-			}
-
-			return jwtToken, http.StatusOK, nil
+			return token, http.StatusOK, nil
 		}
-
 		return "", http.StatusUnauthorized, errors.New("Bad User")
 	})
 
---
+++ internal/pkg/auth/login.go
@@ -0,0 +1,91 @@
+package auth
+
+import (
+	"errors"
+	"os"
+	"ssb/internal/schemas"
+	"ssb/internal/timeutil"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTConfig struct {
+	Iss    string
+	Aud    string
+	TTL    time.Duration
+	Clock  timeutil.Clock
+	Secret string
+}
+
+type JWTOption func(*JWTConfig)
+
+func WithIssuer(iss string) JWTOption {
+	return func(config *JWTConfig) {
+		config.Iss = iss
+	}
+}
+
+func WithAudience(aud string) JWTOption {
+	return func(config *JWTConfig) {
+		config.Aud = aud
+	}
+}
+
+func WithTTL(ttl time.Duration) JWTOption {
+	return func(config *JWTConfig) {
+		config.TTL = ttl
+	}
+}
+
+func WithClock(clock timeutil.Clock) JWTOption {
+	return func(c *JWTConfig) {
+		c.Clock = clock
+	}
+}
+
+func WithSecret(secret string) JWTOption {
+	return func(c *JWTConfig) {
+		c.Secret = secret
+	}
+}
+
+func WithSecretFromEnv(envName string) JWTOption {
+	return func(c *JWTConfig) {
+		c.Secret = os.Getenv(envName)
+	}
+}
+
+func GenerateJWT(
+	username string,
+	opts ...JWTOption,
+) (schemas.JsonToken, error) {
+	c := JWTConfig{
+		TTL:   1 * time.Hour,
+		Clock: timeutil.RealClock{},
+	}
+	for _, opt := range opts {
+		opt(&c)
+	}
+	now := c.Clock.Now().UTC()
+	exp := now.Add(c.TTL)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": username,
+		"iss": c.Iss,
+		"aud": c.Aud,
+		"iat": now.Unix(),
+		"nbf": now.Unix(),
+		"exp": exp.Unix(),
+	})
+	if c.Secret == "" {
+		return schemas.JsonToken{}, errors.New("no secret provided")
+	}
+	tokenString, err := token.SignedString([]byte(c.Secret))
+	if err != nil {
+		return schemas.JsonToken{}, err
+	}
+	jwtToken := schemas.JsonToken{
+		Token: tokenString,
+	}
+	return jwtToken, nil
+}
