d90c1ea3ff1cf5c234389d3426499605e9f5e884
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-09-20 20:07:17 -0500

    outlines setup and healtzh integration test

---
+++ integration/healthz_integration_test.go
@@ -0,0 +1,13 @@
+//go:build integration
+// +build integration
+
+package integration
+
+import (
+	"testing"
+)
+
+func TestHealthzIntegration(t *testing.T){
+	t.Fatal("failed")
+}
+
---
+++ integration/setup.go
@@ -0,0 +1,11 @@
+package integration
+
+func Setup(){
+	// in memory db
+
+	// instantiate all repos
+	
+	// build router
+
+	// return router
+}
