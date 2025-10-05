d3972e313860c8e06a8281c970b485d4b0a01ac0
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-10-05 07:31:47 -0500

    better config

--- internal/app/config.go
+++ internal/app/config.go
@@ -3,25 +3,36 @@ package app
 import (
 	"log"
 	"os"
+	"time"
 )
 
 type Config struct {
-	DBPath string
-	JWTSecret string
+	Port          string
+	DBPath        string
+	JWTSecret     string
 	AdminPassword string
-	Port string
+	JWTIssuer     string
+	JWTAudience   string
+	JWT_TTL       time.Duration
 }
 
-
 func LoadConfig() Config {
-	return Config{}
+	return Config{
+		Port:          getEnv("BFS_PORT", ":8080"),
+		DBPath:        getEnv("BFS_DB_PATH", "./data/ssb.db"),
+		JWTSecret:     mustEnv("BFS_AUTH_SECRET"),
+		AdminPassword: getEnv("BFS_ADMIN_PASSWD", "admin"),
+		JWTIssuer:     getEnv("BFS_JWT_ISSUER", "bfs"),
+		JWTAudience:   getEnv("BFS_JWT_AUDIEN", "bfs"),
+		JWT_TTL:       getEnvDuration("BFS_JWT_TTL", time.Hour),
+	}
 }
 
 func getEnv(key, defaultValue string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
-	return defaultValue 
+	return defaultValue
 }
 
 func mustEnv(key string) string {
@@ -29,6 +40,15 @@ func mustEnv(key string) string {
 	if value == "" {
 		log.Fatalf("missing required env var: %s", key)
 	}
-	return value 
+	return value
 }
 
+func getEnvDuration(key string, defaultValue time.Duration) time.Duration {
+	if value := os.Getenv(key); value != "" {
+		if dur, err := time.ParseDuration(value); err == nil {
+			return dur
+		}
+		log.Fatalf("invalidd duration for %s=%s", key, value)
+	}
+	return defaultValue
+}
