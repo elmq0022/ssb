e70aea5004836d8d7f2140169c5ccf70dd1b0d33
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-10-08 00:25:07 -0500

    starting on the config and token storage and retrival

---
+++ internal/commands/utils/config.go
@@ -0,0 +1,49 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func MustGetConfigDir() string {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		log.Fatalf("could not get config dir: %q", err)
+	}
+	app := "bfs"
+	file := "config.json"
+	return filepath.Join(configDir, app, file)
+}
+
+type CLIConfig struct {
+	URL string `json:"url"`
+	Username string `json:"user"`
+}
+
+func LoadConfig() *CLIConfig {
+	path := MustGetConfigDir()
+	configData, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("couldn't read config: %q", err)
+	}
+
+	var c CLIConfig
+	if err := json.Unmarshal(configData, &c); err != nil {
+		log.Fatalf("could not unmarshal config data: %s", configData)
+	}
+	return &c 
+}
+
+func MustGETCacheDir()
+
+func LoadJWTToken() string {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		log.Fatalf("could not get config dir: %q", err)
+	}
+	app := "bfs"
+	file := "token.json"
+	return filepath.Join(cacheDir, app, file)
+}
---
+++ internal/commands/utils/config_test.go
@@ -0,0 +1,20 @@
+package utils_test
+
+import (
+	"os"
+	"path/filepath"
+	"ssb/internal/commands/utils"
+	"testing"
+)
+
+func TestMustGetConfigDir(t *testing.T) {
+    home, _ := os.UserHomeDir()
+	configDir := ".config"
+	app := "bfs"
+	file := "config.json"
+	want := filepath.Join(home, configDir, app, file)
+	got :=  utils.MustGetConfigDir()
+	if got != want {
+		t.Fatalf("want %s, got %s", want, got)
+	}
+}
