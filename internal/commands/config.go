b949f982083872e9f0652b92b4db05fd95909f23
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-10-13 14:16:38 -0500

    adds additional config commands

--- internal/commands/config.go
+++ internal/commands/config.go
@@ -12,25 +12,43 @@ func HandleConfig(args []string) {
 	case "init":
 		handleInit()
 	case "set-server":
-		fmt.Println("hello from set-server")
+		handleSetServer()
 	case "set-user":
-		fmt.Println("hello from set-user")
+		handleSetUser()
 	default:
 		fmt.Println("expected one of 'set-server' or 'set-user'")
 	}
 }
 
-func handleInit(){
-	cfg := utils.CLIConfig{}
+func handleInit() {
+	cfg := &utils.CLIConfig{}
 
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("Enter bfs url: ")
 	scanner.Scan()
 	cfg.URL = scanner.Text()
 
-	fmt.Print("Enter your username: ")
+	fmt.Print("Enter your user name: ")
 	scanner.Scan()
 	cfg.Username = scanner.Text()
 
-	utils.MustSetConfig(&cfg)
+	utils.MustSetConfig(cfg)
+}
+
+func handleSetServer() {
+	cfg := utils.MustReadConfig()
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter bfs url: ")
+	scanner.Scan()
+	cfg.URL = scanner.Text()
+	utils.MustSetConfig(cfg)
+}
+
+func handleSetUser() {
+	cfg := utils.MustReadConfig()
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter your user name: ")
+	scanner.Scan()
+	cfg.Username = scanner.Text()
+	utils.MustSetConfig(cfg)
 }
