1ac6a0ba8783deedfdd6ffe0daaae40c3bbf004d
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-10-12 20:37:23 -0500

    initialize the cli config

--- internal/commands/config.go
+++ internal/commands/config.go
@@ -1,9 +1,16 @@
 package commands
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"ssb/internal/commands/utils"
+)
 
 func HandleConfig(args []string) {
 	switch args[0] {
+	case "init":
+		handleInit()
 	case "set-server":
 		fmt.Println("hello from set-server")
 	case "set-user":
@@ -11,4 +18,19 @@ func HandleConfig(args []string) {
 	default:
 		fmt.Println("expected one of 'set-server' or 'set-user'")
 	}
+}
+
+func handleInit(){
+	cfg := utils.CLIConfig{}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter bfs url: ")
+	scanner.Scan()
+	cfg.URL = scanner.Text()
+
+	fmt.Print("Enter your username: ")
+	scanner.Scan()
+	cfg.Username = scanner.Text()
+
+	utils.MustSetConfig(&cfg)
 }
